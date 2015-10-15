dbbd1261568c3d6b638cf1d8f31c3ab500e74341
carlosef <carlosef1976@hotmail.com>
2015-10-14 23:20:55 -0600

    estructura de figuras (eclipse)

---
+++ tareas/Figura/Figuras.java
@@ -0,0 +1,12 @@
+
+public class Figuras {
+
+	public static void main(String[] args) {
+		
+		System.out.println("estructura de la tarea de figuras ");
+		
+     
+		Poligono objPoligono = new Poligono() ;
+	}
+
+}
---
+++ tareas/Figura/src/Poligono.java
@@ -0,0 +1,29 @@
+
+public class Poligono {
+      
+	    private int numLados;
+		private double puntox ;
+		private double puntoy ;
+		public int getNumLados() {
+			return numLados;
+		}
+		public void setNumLados(int numLados) {
+			this.numLados = numLados;
+		}
+		public double getPuntox() {
+			return puntox;
+		}
+		public void setPuntox(double puntox) {
+			this.puntox = puntox;
+		}
+		public double getPuntoy() {
+			return puntoy;
+		}
+		public void setPuntoy(double puntoy) {
+			this.puntoy = puntoy;
+		}
+		
+		
+	}
+
+
